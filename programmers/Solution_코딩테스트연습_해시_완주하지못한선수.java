2cf0674bc59804e962c335e765c48735402d4195
jeongho <qkrjh0904@gmail.com>
2023-04-16 23:30:45 +0900

    코딩테스트연습_해시_완주하지못한선수.java

--- programmers/Solution_코딩테스트연습_해시_완주하지못한선수.java
+++ programmers/Solution_코딩테스트연습_해시_완주하지못한선수.java
@@ -1,30 +1,28 @@
 package programmers;
 
-import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Set;
 
 public class Solution_코딩테스트연습_해시_완주하지못한선수 {
     public String solution(String[] participant, String[] completion) {
 
         HashMap<String, Integer> map = new HashMap<>();
         for (String name : participant) {
-            if (map.containsKey(name)) {
-                map.put(name, map.get(name) + 1);
-                continue;
-            }
-            map.put(name, 1);
+            map.put(name, map.getOrDefault(name, 0) + 1);
         }
-
         for (String name : completion) {
-            if (map.containsKey(name)) {
-                if (map.get(name).equals(1)) {
-                    map.remove(name);
-                    continue;
-                }
-                map.put(name, map.get(name) - 1);
+            map.put(name, map.get(name) - 1);
+        }
+
+        String answer = "";
+        Set<String> keySet = map.keySet();
+        for (String name : keySet) {
+            if (map.get(name).equals(1)) {
+                answer = name;
+                break;
             }
         }
 
-        return new ArrayList<>(map.keySet()).get(0);
+        return answer;
     }
 }
