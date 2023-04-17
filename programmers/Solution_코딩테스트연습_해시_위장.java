df8694ac601f2fa8843349e2e59be0cc55349809
jeongho <qkrjh0904@gmail.com>
2023-04-18 00:06:45 +0900

    Solution_코딩테스트연습_해시_위장.java

--- programmers/Solution_코딩테스트연습_해시_위장.java
+++ programmers/Solution_코딩테스트연습_해시_위장.java
@@ -2,9 +2,11 @@ package programmers;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class Solution_코딩테스트연습_해시_위장 {
 
+    // (1번 의상개수 + 1) * (2번 의상개수 + 1) * (3번 의상개수 + 1) * ... * (n번 의상개수 + 1) -1
     public static int solution(String[][] clothes) {
 
         HashMap<String, Integer> map = new HashMap<>();
@@ -13,12 +15,13 @@ public class Solution_코딩테스트연습_해시_위장 {
             map.put(type, map.getOrDefault(type, 0) + 1);
         }
 
-        ArrayList<Integer> list = new ArrayList<>(map.values());
+        int answer = 1;
+        List<Integer> list = new ArrayList<>(map.values());
+        for (Integer num : list) {
+            answer *= (num + 1);
+        }
 
-
-
-        int answer = 0;
-        return answer;
+        return answer - 1;
     }
 
     public static void main(String[] args) {
