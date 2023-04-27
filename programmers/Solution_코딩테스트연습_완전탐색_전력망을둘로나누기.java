863edc98f64f2993be2e0d8ddce169064845a2f2
jeongho <qkrjh0904@gmail.com>
2023-04-27 23:51:59 +0900

    Solution_코딩테스트연습_완전탐색_전력망을둘로나누기.java

---
+++ programmers/Solution_코딩테스트연습_완전탐색_전력망을둘로나누기.java
@@ -0,0 +1,20 @@
+package programmers;
+
+public class Solution_코딩테스트연습_완전탐색_전력망을둘로나누기 {
+
+    public static int solution(int n, int[][] wires) {
+        int answer = -1;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int n = 9;
+        int[][] wires = {{1, 3}, {2, 3}, {3, 4}, {4, 5}, {4, 6}, {4, 7}, {7, 8}, {7, 9}};
+//        int n = 4;
+//        int[][] wires = {{1,2},{2,3},{3,4}};
+//        int n = 7;
+//        int[][] wires = {{1,2},{2,7},{3,7},{3,4},{4,5},{6,7}};
+        solution(9, wires);
+    }
+
+}
