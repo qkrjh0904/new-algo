064ae0ccca58d536d6614ed921c2be7c69a340af
jeongho <qkrjh0904@gmail.com>
2023-06-30 00:16:09 +0900

    Solution_코딩테스트연습_완전탐색_전력망을둘로나누기.java

--- programmers/Solution_코딩테스트연습_완전탐색_전력망을둘로나누기.java
+++ programmers/Solution_코딩테스트연습_완전탐색_전력망을둘로나누기.java
@@ -5,76 +5,76 @@ import java.util.Arrays;
 public class Solution_코딩테스트연습_완전탐색_전력망을둘로나누기 {
 
     public static int answer = 100;
-    public static int[] parent;
+    public static boolean[] visit;
+
+    public static int[][] map;
 
     public static void main(String[] args) {
-//        int n = 9;
-//        int[][] wires = {{1, 3}, {2, 3}, {3, 4}, {4, 5}, {4, 6}, {4, 7}, {7, 8}, {7, 9}};
+        int n = 9;
+        int[][] wires = {{1, 3}, {2, 3}, {3, 4}, {4, 5}, {4, 6}, {4, 7}, {7, 8}, {7, 9}};
 //        int n = 4;
-//        int[][] wires = {{1,2},{2,3},{3,4}};
+//        int[][] wires = {{1, 2}, {2, 3}, {3, 4}};
 //        int n = 2;
 //        int[][] wires = {{1, 2}};
-        int n = 7;
-        int[][] wires = {{1, 2}, {2, 7}, {3, 7}, {3, 4}, {4, 5}, {6, 7}};
+//        int n = 7;
+//        int[][] wires = {{1, 2}, {2, 7}, {3, 7}, {3, 4}, {4, 5}, {6, 7}};
         solution(n, wires);
     }
 
     public static int solution(int n, int[][] wires) {
         for (int i = 0; i < wires.length; ++i) {
-            go(i, n, wires);
+            visit = new boolean[n];
+            setMap(i, n, wires);
+//            print(n);
+            dfs(0, n);
+            int count = count();
+            answer = Math.min(Math.abs(count - (n - count)), answer);
         }
-
+        System.out.println(answer);
         return answer;
     }
 
-    private static void go(int index, int n, int[][] wires) {
-        parent = new int[n];
-        for (int i = 0; i < n; ++i) {
-            parent[i] = i;
-        }
-
-        for (int i = 0; i < wires.length; ++i) {
-            if (i == index)
-                continue;
-
-            int node1 = wires[i][0] - 1;
-            int node2 = wires[i][1] - 1;
-
-            union(node1, node2);
-        }
-        count();
-    }
-
-    private static void count() {
-        int p1 = parent[0];
+    private static int count() {
         int cnt = 0;
-        for (int i = 0; i < parent.length; ++i) {
-            if (parent[i] == p1) {
-                cnt++;
-            } else {
-                cnt--;
-            }
+        for (boolean b : visit) {
+            if (b) cnt++;
         }
-        System.out.println(Math.abs(cnt));
-        answer = Math.min(answer, Math.abs(cnt));
+        return cnt;
     }
 
-    private static void union(int node1, int node2) {
-        int p1 = find(node1);
-        int p2 = find(node2);
-
-        if (p1 < p2) {
-            parent[p2] = p1;
+    private static void dfs(int i, int n) {
+        if (visit[i]) {
             return;
         }
-        parent[p1] = p2;
+
+        visit[i] = true;
+        for (int j = 0; j < n; ++j) {
+            if (i == j) continue;
+
+            if (!visit[j] && map[i][j] == 1) {
+                dfs(j, n);
+            }
+        }
     }
 
-    private static int find(int node) {
-        if (parent[node] == node) {
-            return node;
+    private static void setMap(int except, int n, int[][] wires) {
+        map = new int[n][n];
+
+        for (int i = 0; i < wires.length; ++i) {
+            if (i == except) continue;
+
+            int n1 = wires[i][0] - 1;
+            int n2 = wires[i][1] - 1;
+
+            map[n1][n2] = 1;
+            map[n2][n1] = 1;
         }
-        return parent[node] = find(parent[node]);
+    }
+
+    private static void print(int n) {
+        for (int i = 0; i < n; ++i) {
+            System.out.println(Arrays.toString(map[i]));
+        }
     }
 
 }
