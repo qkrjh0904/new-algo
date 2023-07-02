a959fe88792a4661b4e9f65e32c03a766058e56e
jeongho <qkrjh0904@gmail.com>
2023-07-02 23:04:05 +0900

    Solution_코딩테스트연습_깊이너비우선탐색_단어변환.java

---
+++ programmers/Solution_코딩테스트연습_깊이너비우선탐색_단어변환.java
@@ -0,0 +1,16 @@
+package programmers;
+
+public class Solution_코딩테스트연습_깊이너비우선탐색_단어변환 {
+
+    public static void main(String[] args) {
+        String begin = "hit";
+        String target = "cog";
+        String[] words = {"hot", "dot", "dog", "lot", "log", "cog"};
+        solution(begin, target, words);
+    }
+
+    public static int solution(String begin, String target, String[] words) {
+        int answer = 0;
+        return answer;
+    }
+}
